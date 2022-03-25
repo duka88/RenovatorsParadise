ab9ef12ba834c0a02286c0c219afb9d8e131075f
Mirjana Perić <mirjana.orlic4@gmail.com>
2022-03-25 21:10:42 +0100

    Add location section

---
+++ components/home/location.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="location">
+        <div class="wrap">
+            <div class="location__heading-wrap">
+                <h2 class="location__heading">SHOWROOM</h2>
+                <svg class="location__down-icon">
+                    <use xlink:href="~/assets/svg/sprite.svg#chevron-down-solid"></use>
+                </svg>
+            </div>
+            <div class="location-top">
+                <svg class="location__icon">
+                    <use xlink:href="~/assets/svg/sprite.svg#location-dot-solid"></use>
+                </svg>
+                <div class="location-top__wrap">
+                    <h4 class="location-top__heading">
+                        VISIT OUR SHOWROOM
+                    </h4>
+                    <p class="location-top__text">32 Futura Road Keysborough 3173</p>
+                </div>
+            </div>
+            <div class="location__iframe"></div>
+            <div class="location__heading-wrap">
+                <h2 class="location__heading">shipping</h2>
+                <svg class="location__down-icon">
+                    <use xlink:href="~/assets/svg/sprite.svg#chevron-down-solid"></use>
+                </svg>
+            </div>
+             <div class="location__bottom">
+                <p class="location__text">We deliver to Melbourne and surrounding areas. Input your address and calculate shipping. The price allows for one delivery person and any number of items. Timber or Heavy items may incur an extra 20% fee. REGIONAL or INTERSTATE transport options are also listed on-click.</p>
+                 <button class="btn btn--dark location__btn">
+                    <svg class="location__icon">
+                        <use xlink:href="~/assets/svg/sprite.svg#truck-fast-solid"></use>
+                    </svg>
+                    SHIPPING ESTIMATOR
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'location'
+}
+</script>
--- pages/product.vue
+++ pages/product.vue
@@ -2,6 +2,7 @@
   <main class="main main--product">
     <Breadcrumbs />
     <ProductSection />
+    <Location />
     <NewIn title="WE ALSO RECOMMEND" />
     <NewIn title="YOU MAY ALSO LIKE" />
     <Contact />
@@ -13,12 +14,14 @@ import Contact from "@/components/home/contact.vue";
 import NewIn from "@/components/home/new-in.vue";
 import ProductSection from "@/components/home/product-section.vue";
 import Breadcrumbs from "@/components/breadcrumbs/main.vue";
+import Location from "@/components/home/location.vue";
 
 export default {
 
   name: 'Product',
   components: {
       Contact,
+      Location,
       NewIn,
       Breadcrumbs,
       ProductSection
