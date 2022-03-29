3f439501885a0ea22e898c6d297d497d31e88feb
Mirjana Perić <mirjana.orlic4@gmail.com>
2022-03-29 21:39:33 +0200

    Add table

--- components/product-table/main.vue
+++ components/product-table/main.vue
@@ -1,36 +1,117 @@
 <template>
-    <table class="product-table">
-      <thead>
-          <tr>
-              <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
-              <td class="product-table__head">Details</td>
-              <td class="product-table__head">Size (mm)</td>
-              <td class="product-table__head">specs</td>
-              <td class="product-table__head">whishlist</td>
-              <td class="product-table__head">stock#</td>
-              <td class="product-table__head">price</td>
-              <td class="product-table__head">enquire</td>
-          </tr>
-      </thead>
-      <tbody>
-          <tr>
-              <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
-              <td><p class="product-table__text">Recycled Timber Mantle Enquiry Text</p></td>
-              <td><p class="product-table__text">1285 width x 1780 height</p></td>
-              <td><p class="product-table__text">268 L / M</p></td>
-              <td>  
-                <svg class="product-section__small-icon">
-                    <use xlink:href="~/assets/svg/sprite.svg#heart-solid"></use>
-                </svg>
-              </td>
-              <td class="product-table__bold">#F287</td>
-              <td class="product-table__bold">$375.00</td>
-              <td>
-                   <input type="checkbox" id="check" name="check">
-              </td>
-          </tr>
-      </tbody>
-    </table>
+    <div class="peoduct-table-wrap">
+        <table class="product-table">
+        <thead>
+            <tr>
+                <td><img class="product-table__img" src="~/assets/img/img.png" alt="Shop item"></td>
+                <td class="product-table__head">Details</td>
+                <td class="product-table__head">Size (mm)</td>
+                <td class="product-table__head">specs</td>
+                <td class="product-table__head">whishlist</td>
+                <td class="product-table__head">stock#</td>
+                <td class="product-table__head">price</td>
+                <td class="product-table__head">enquire</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
+                <td><p class="product-table__text">Recycled Timber Mantle Enquiry Text</p></td>
+                <td><p class="product-table__text">1285 width x 1780 height</p></td>
+                <td><p class="product-table__text">268 L / M</p></td>
+                <td>  
+                    <svg class="product-section__small-icon">
+                        <use xlink:href="~/assets/svg/sprite.svg#heart-solid"></use>
+                    </svg>
+                </td>
+                <td class="product-table__bold">#F287</td>
+                <td class="product-table__bold">$375.00</td>
+                <td>
+                    <label class="product-table__container">
+                        <input type="checkbox" checked="checked">
+                        <span class="product-table__checkmark"></span>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
+                <td><p class="product-table__text">Recycled Timber Mantle Enquiry Text</p></td>
+                <td><p class="product-table__text">1285 width x 1780 height</p></td>
+                <td><p class="product-table__text">268 L / M</p></td>
+                <td>  
+                    <svg class="product-section__small-icon">
+                        <use xlink:href="~/assets/svg/sprite.svg#heart-solid"></use>
+                    </svg>
+                </td>
+                <td class="product-table__bold">#F287</td>
+                <td class="product-table__bold">$375.00</td>
+                <td>
+                    <label class="product-table__container">
+                        <input type="checkbox" checked="checked">
+                        <span class="product-table__checkmark"></span>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
+                <td><p class="product-table__text">Recycled Timber Mantle Enquiry Text</p></td>
+                <td><p class="product-table__text">1285 width x 1780 height</p></td>
+                <td><p class="product-table__text">268 L / M</p></td>
+                <td>  
+                    <svg class="product-section__small-icon">
+                        <use xlink:href="~/assets/svg/sprite.svg#heart-solid"></use>
+                    </svg>
+                </td>
+                <td class="product-table__bold">#F287</td>
+                <td class="product-table__bold">$375.00</td>
+                <td>
+                    <label class="product-table__container">
+                        <input type="checkbox" checked="checked">
+                        <span class="product-table__checkmark"></span>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
+                <td><p class="product-table__text">Recycled Timber Mantle Enquiry Text</p></td>
+                <td><p class="product-table__text">1285 width x 1780 height</p></td>
+                <td><p class="product-table__text">268 L / M</p></td>
+                <td>  
+                    <svg class="product-section__small-icon">
+                        <use xlink:href="~/assets/svg/sprite.svg#heart-solid"></use>
+                    </svg>
+                </td>
+                <td class="product-table__bold">#F287</td>
+                <td class="product-table__bold">$375.00</td>
+                <td>
+                    <label class="product-table__container">
+                        <input type="checkbox" checked="checked">
+                        <span class="product-table__checkmark"></span>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td><img class="product-table__img" src="~/assets/img/product-slider-image.png" alt="Shop item"></td>
+                <td><p class="product-table__text">Recycled Timber Mantle Enquiry Text</p></td>
+                <td><p class="product-table__text">1285 width x 1780 height</p></td>
+                <td><p class="product-table__text">268 L / M</p></td>
+                <td>  
+                    <svg class="product-section__small-icon">
+                        <use xlink:href="~/assets/svg/sprite.svg#heart-solid"></use>
+                    </svg>
+                </td>
+                <td class="product-table__bold">#F287</td>
+                <td class="product-table__bold">$375.00</td>
+                <td>
+                    <label class="product-table__container">
+                        <input type="checkbox" checked="checked">
+                        <span class="product-table__checkmark"></span>
+                    </label>
+                </td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
